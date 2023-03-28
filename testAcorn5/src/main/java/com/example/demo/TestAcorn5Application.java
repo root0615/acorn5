093701a908e7bf78a524d512e0406b547c22f84e
GROOT <yungun4101@gmail.com>
2023-03-28 14:36:00 +0900

    test

---
+++ testAcorn5/src/main/java/com/example/demo/TestAcorn5Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAcorn5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAcorn5Application.class, args);
+	}
+
+}
